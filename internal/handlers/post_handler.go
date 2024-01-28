b950ee4a152876b94fd26d237f888b5225f13fad
Platon <xnpltn@gmail.com>
2024-01-28 03:30:43 +0200

    file aploads, DRYing posts.go file

--- internal/handlers/post_handler.go
+++ internal/handlers/post_handler.go
@@ -2,41 +2,22 @@ package handlers
 
 import (
 	"log"
-	// "fmt"
 	"net/http"
-	// "encoding/json"
 	"github.com/xnpltn/codegram/internal/models"
 	"github.com/xnpltn/codegram/internal/utils"
 	"github.com/gorilla/mux"
 )
 
 func CreatePosts(w http.ResponseWriter, r *http.Request, user models.DBUser){
-	// sqlStatement := `
-	// 	INSERT INTO posts (user_id, image_url, description)
-	// 	VALUES ($1, $2, $3)
-	// 	RETURNING id, user_id, image_url, description;
-	// `
 	db, err := models.InitDB()
 	if err != nil {
 		log.Fatal("Error initiating database", err)
 	}
 	defer db.Close()
-	
-	// decoder := json.NewDecoder(r.Body)
-	// params := models.CreatePostParams{}
-	// post := models.DBPost{}
-	// error := decoder.Decode(&params)
-	// if error != nil {
-	// 	utils.RespondWithError(w, 400, fmt.Sprintf("Error Persing JSON: %v", error))
-	// 	return
-	// }
-	
-	// err = db.QueryRow(sqlStatement, user.ID, params.ImageURL, params.Description).Scan(&post.ID, &post.UserID, &post.ImageURL, &post.Description)
-	
-	// if err != nil{
-	// 	log.Fatal("error", err)
-	// }
 	post, err := utils.CreatePost(w, r, db, user)
+	if err != nil {
+		utils.RespondWithError(w, 404, err.Error())
+	}
 	utils.RespondWithJson(w, 201, post)
 	
 }
--- internal/utils/posts.go
+++ internal/utils/posts.go
@@ -4,28 +4,46 @@ import (
 	"database/sql"
 	"log"
 	"github.com/xnpltn/codegram/internal/models"
-	"encoding/json"
 	"net/http"
 	"fmt"
+	"os"
+	"io"
 )
 
 
 
-func CreatePost(w http.ResponseWriter, r *http.Request, db *sql.DB, user models.DBUser) (models.DBPost, error){
+func CreatePost(_ http.ResponseWriter, r *http.Request, db *sql.DB, user models.DBUser) (models.DBPost, error){
+	r.ParseMultipartForm(20 * 1024 * 1024)
 	sqlStatement := `
 		INSERT INTO posts (user_id, image_url, description)
 		VALUES ($1, $2, $3)
 		RETURNING id, user_id, image_url, description;
 	`
-	decoder := json.NewDecoder(r.Body)
-	params := models.CreatePostParams{}
-	post := models.DBPost{}
-	error := decoder.Decode(&params)
-	if error != nil {
-		RespondWithError(w, 400, fmt.Sprintf("Error Persing JSON: %v", error))
-	}
 	
-	err := db.QueryRow(sqlStatement, user.ID, params.ImageURL, params.Description).Scan(&post.ID, &post.UserID, &post.ImageURL, &post.Description)
+	file, _, err := r.FormFile("image")
+
+	if err != nil{
+		fmt.Println("error occured", err)
+	}
+
+	imageDescription := r.FormValue("description")
+	defer file.Close()
+	tempfile, err := os.CreateTemp("uploads", "uplaod-*.jpg")
+	if err != nil{
+		fmt.Println("error occured", err)
+	}
+	fileBytes, err := io.ReadAll(file)
+	if err != nil{
+		fmt.Println("error occured", err)
+	}
+	if err != nil{
+		fmt.Println("error occured", err)
+	}
+	tempfile.Write(fileBytes)
+	post := models.DBPost{}
+	imageUrl := tempfile.Name()
+
+	err = db.QueryRow(sqlStatement, user.ID, imageUrl, imageDescription).Scan(&post.ID, &post.UserID, &post.ImageURL, &post.Description)
 	
 	if err != nil{
 		log.Fatal("error", err)
@@ -39,19 +57,10 @@ func GetAllPosts(db *sql.DB) ([]models.DBPost, error){
 		log.Fatal(err)
 	}
 
-	items := []models.DBPost{}
-	
-	for rows.Next(){
-		var post models.DBPost
-		if err := rows.Scan(
-			&post.ID,
-			&post.UserID,
-			&post.ImageURL,
-			&post.Description,
-		); err != nil {
-			log.Fatal("error", err)
-		}
-		items = append(items, post)
+	items, err := getPostItemsFromDB(rows)
+
+	if err != nil {
+		log.Fatal("error", err)
 	}
 
 	return items, nil
@@ -67,18 +76,10 @@ func GetPost(db *sql.DB, id string) ([]models.DBPost, error){
 		log.Fatal("error", err)
 	}
 
-	items := []models.DBPost{}
-	for rows.Next(){
-		var post models.DBPost
-		if err := rows.Scan(
-			&post.ID,
-			&post.UserID,
-			&post.ImageURL,
-			&post.Description,
-		); err != nil {
-			log.Fatal("error", err)
-		}
-		items = append(items, post)
+	items, err := getPostItemsFromDB(rows)
+
+	if err != nil {
+		log.Fatal("error", err)
 	}
 	return items, nil
 }
@@ -93,7 +94,16 @@ func DeletePost(db *sql.DB, id string, user *models.DBUser) ([]models.DBPost, er
 	if err != nil {
 		log.Fatal("error", err)
 	}
+	items, err := getPostItemsFromDB(rows)
 
+	if err != nil {
+		log.Fatal("error", err)
+	}
+
+	return items, nil
+}
+
+func getPostItemsFromDB(rows *sql.Rows) ([]models.DBPost, error){
 	items := []models.DBPost{}
 	for rows.Next(){
 		var post models.DBPost
@@ -107,5 +117,6 @@ func DeletePost(db *sql.DB, id string, user *models.DBUser) ([]models.DBPost, er
 		}
 		items = append(items, post)
 	}
+
 	return items, nil
 }
